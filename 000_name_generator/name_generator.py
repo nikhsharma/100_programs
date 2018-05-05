394065ce3b93f44e8e8eda1c93e45d17f250a6b7
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-05 23:10:51 +0100

    Refactor import to make more efficient

--- 000_name_generator/name_generator.py
+++ 000_name_generator/name_generator.py
@@ -1,11 +1,11 @@
-import random
+from random import choice
 
 
 first_name = ["Henry", "Lucy", "Andy", "Mike", "Sarah", "Frank", "Jade", "Shaun", "Louise", "Sandra", "Tom"]
 second_name = ["O'Conner", "Smith", "Turner", "Sturrock", "McGhee", "Butler", "Grey", "Thompson"]
 
 def return_random_name():
-    print random.choice(first_name) + " " +  random.choice(second_name)
+    print choice(first_name) + " " +  choice(second_name)
 exit = False
 
 while exit == False:
