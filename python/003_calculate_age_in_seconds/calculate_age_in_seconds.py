a11c030be90bf4a7885327ca00f3e32d41b43340
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-16 23:24:54 +0100

    Refactor calculate age in seconds to be accurate to the day

--- python/003_calculate_age_in_seconds/calculate_age_in_seconds.py
+++ python/003_calculate_age_in_seconds/calculate_age_in_seconds.py
@@ -1,2 +1,36 @@
-age = input('how old are you?')
-print("You are " + str(age*31536000) + " seconds old")
+import datetime
+
+
+def print_header():
+    print('-----------------------------------')
+    print('   HOW OLD ARE YOU IN SECONDS?')
+    print('-----------------------------------')
+
+
+def get_bday_from_user():
+    print('When were you born? ')
+    year = int(input("Year [YYYY]: "))
+    month = int(input("Month [MM]: "))
+    day = int(input("Day [DD]: "))
+
+    return datetime.date(year, month, day)
+
+
+def calculate_age_in_seconds(original_date, target_date):
+    dt = original_date - target_date
+    return -dt.days * 86400
+
+
+def print_results(seconds):
+    print("Looks like you\'re {} seconds old!".format(seconds))
+
+
+def main():
+    print_header()
+    bday = get_bday_from_user()
+    now = datetime.date.today()
+    seconds = calculate_age_in_seconds(bday, now)
+    print_results(seconds)
+
+
+main()
